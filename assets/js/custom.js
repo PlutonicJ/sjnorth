363865716474e9b7d3b8cf04ccd46c14e29868e3
cybi <jens@koetterheinri.ch>
2021-07-01 23:28:52 +0200

    init website

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+// Put custom JavaScript code in this file
+
+$(window).scroll(function () {
+    $('.navbar').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (topOfWindow > 40) {
+            $(this).addClass("sticky");
+        } else {
+            $(this).removeClass("sticky");
+        }
+    });
+});
