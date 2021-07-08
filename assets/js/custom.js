05ae01e19bc50360b1336a2b5cdaef5ddfbbbf15
cybi <jens@koetterheinri.ch>
2021-07-09 00:09:18 +0200

    added sticky header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,14 @@
 // Put custom JavaScript code in this file
+$(window).scroll(function () {
+    $('.navigation').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (topOfWindow > 40) {
+            $(this).addClass("sticky");
+        } else {
+            $(this).removeClass("sticky");
+        }
+    });
+});
